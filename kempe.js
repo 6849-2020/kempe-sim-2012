3da7b2e8e88b28d7bf04323278f2e411ba4cf126
DarkArchon <darkarchon2@gmail.com>
2012-11-17 14:32:44 -0500

    some drawing and mouse code

--- kempe.js
+++ kempe.js
@@ -1,57 +1,394 @@
-var canvas;
-var cx;
+var cvs;
+var ctx;
 var errordisplay;
 
-function draw() {
-    cx.fillStyle = Colors.rand();
-    cx.fillRect(pos[0],pos[1],1,1);
-    
+var starttime = new Date().getTime();
+
+var objs = [];
+var ply = {};
+var NUM_OBJECTS = 10;
+var FPS = 60.0/3;
+var PLY_SPEED = 100.0/FPS;
+var TILE_WIDTH = 50;
+var map = {};
+var view = {};
+var VIEW_WIDTH = 500;
+var VIEW_HEIGHT = 450;
+var VIEW_TILE_WIDTH = VIEW_WIDTH/TILE_WIDTH;
+var VIEW_TILE_HEIGHT = VIEW_HEIGHT/TILE_WIDTH;
+var VIEW_TILE_WIDTH_HALF = (VIEW_TILE_WIDTH/2)>>0;
+var VIEW_TILE_HEIGHT_HALF = (VIEW_TILE_HEIGHT/2)>>0;
+var data;
+var selected;
+var hilight;
+var dragging;
+var lastpos;
+var RADIUS = 10;
+var cx;
+var cy;
+var sx;
+var sy;
+
+function init() {
+    data = 
+    [
+        [
+            [0  ,   0   ,   true],
+            [0  ,   100 ,   false],
+            [100,   0   ,   false],
+            [100,   100 ,   false]
+        ],
+        [
+            [0, 1,  1],
+            [0, 2,  1],
+            [0, 3,  Math.sqrt(2)],
+            [1, 2,  Math.sqrt(2)],
+            [1, 3,  1],
+            [2, 3,  1]
+        ]
+    ];
+    var data1 = 
+    [
+        [
+            [0  ,   0   ,   true],
+            [0  ,   100 ,   false]
+        ],
+        [
+            [0, 1,  100],
+        ]
+    ];
+
+    var data2 = 
+    [
+        [
+            [0  ,   0   ,   true],
+            [0  ,   100 ,   false],
+            [100,   0   ,   false],
+            [100,   100 ,   false]
+        ],
+        [
+            [0, 1,  100],
+            [0, 2,  100],
+            [2, 3,  100],
+            [1, 3,  100],
+        ]
+    ];
+
+    data = data1;
+
+
+    selected = false;
+    dragging = false;
+    hilight = false;
+    sx = 1;
+    sy = -1;
+    cx = VIEW_WIDTH/2;
+    cy = VIEW_HEIGHT/2;
+    lastpos = [0,0];
 }
 
-var pos;
-var corners;
+function draw() {
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0, 0, VIEW_WIDTH, VIEW_HEIGHT);
+
+    ctx.fillStyle = 'blue';
+    ctx.strokeStyle = 'blue';
+
+    
+    for (var i=0; i<data[1].length; i++)
+    {
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = 'black';
+        ctx.beginPath();
+        ctx.moveTo(cx+sx*data[0][data[1][i][0]][0],cy+sy*data[0][data[1][i][0]][1]);
+        ctx.lineTo(cx+sx*data[0][data[1][i][1]][0],cy+sy*data[0][data[1][i][1]][1]);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = 'blue';
+        ctx.beginPath();
+        ctx.moveTo(cx+sx*data[0][data[1][i][0]][0],cy+sy*data[0][data[1][i][0]][1]);
+        ctx.lineTo(cx+sx*data[0][data[1][i][1]][0],cy+sy*data[0][data[1][i][1]][1]);
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    for (var i=data[0].length-1; i>=0; i--)
+    {
+
+        if (selected == i+1 || hilight == i+1)
+            if (data[0][i][2])
+                ctx.fillStyle = '#FF8888'
+            else
+                ctx.fillStyle = '#88FF88';
+        else
+            if (data[0][i][2])
+                ctx.fillStyle = 'red'
+            else
+                ctx.fillStyle = 'green';
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'black';
+        ctx.beginPath();
+        ctx.arc((cx+sx*data[0][i][0]), 
+                (cy+sy*data[0][i][1]), 
+                RADIUS/2, 0, Math.PI*2, true);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    }
+}
+
 function update() {
-    var tmp = corners[Math.floor(Math.random()*3)];
-    pos[0] = (pos[0]+tmp[0])/2;
-    pos[1] = (pos[1]+tmp[1])/2;
+    handleKeys();
 }
 
 
 function kempeStart() {
-    canvas = document.getElementById("graphics-canvas");
+    cvs = document.getElementById("graphics-canvas");
     errordisplay = document.getElementById("error-display");
-    cx = canvas.getContext("2d");
-    cx.fillStyle = "#FFFFFF";
-    cx.fillRect(0,0,500,500);
-    cx.fillStyle = "#000000";
+    ctx = cvs.getContext("2d");
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0,0,500,500);
+    ctx.fillStyle = "#000000";
     pos = [0,0];
     corners = [ [0,0],
                 [500,0],
                 [0,500]];
 
-    // tick();
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+
+    cvs.addEventListener('mousedown', handleMouseDown);
+    cvs.addEventListener('mouseup', handleMouseUp);
+    cvs.addEventListener('mousemove', handleMouseMove);
+    cvs.addEventListener('mousewheel', handleMouseWheel);
+    cvs.addEventListener('mousemove', handleMouseOver);
+    cvs.addEventListener('mouseout', handleMouseOut);
+
+
+    cvs.onselectstart = function () { return false; } // ie
+    cvs.onmousedown = function () { return false; } // mozilla
+
+    ctx.fillStyle = 'red';
+
+    init();
+    tick();
+}
+
+function handleMouseOver(e) {
+    if (selected == false && hilight == false)
+    {
+        var mx = (e.offsetX-cx)/sx;
+        var my = (e.offsetY-cy)/sy;
+        for (var i=0; i<data[0].length; i++)
+        {
+            if (data[0][i][2])
+                continue;
+            if ((Math.abs(data[0][i][0]-mx) <= RADIUS/2.0/sx) && (Math.abs(data[0][i][1]-my) <= RADIUS/2.0/Math.abs(sy)))
+            {
+                hilight = i+1;
+                lastpos[0] = mx;
+                lastpos[1] = my;
+                break;
+            }
+        }
+    } else if (selected != false)
+        hilight = selected;
+    else if (hilight != false)
+    {
+        i = hilight-1;
+        if (!((Math.abs(data[0][i][0]-mx) <= RADIUS/2.0/sx) && (Math.abs(data[0][i][1]-my) <= RADIUS/2.0/Math.abs(sy))))
+        {
+            hilight = false;
+        }
+    }
+}
+
+function handleMouseWheel(e) {
+    if (e.wheelDelta)
+    {
+        var w = e.wheelDelta/120;
+        console.log(w);
+        if (w>0)
+        {
+            for (var i=0; i<w; i++)
+            {
+                sx /= 0.9;
+                sy /= 0.9;
+                cx = VIEW_WIDTH/2 + (cx-VIEW_WIDTH/2)/0.9
+                cy = VIEW_HEIGHT/2 + (cy-VIEW_HEIGHT/2)/0.9
+            }
+        } else
+        {
+            for (var i=0; i<-w; i++)
+            {
+                sx *= 0.9;
+                sy *= 0.9;
+                cx = VIEW_WIDTH/2 + (cx-VIEW_WIDTH/2)*0.9
+                cy = VIEW_HEIGHT/2 + (cy-VIEW_HEIGHT/2)*0.9
+            }
+        }
+    }
+}
+
+function handleMouseDown(e) {
+    var mx = (e.offsetX-cx)/sx;
+    var my = (e.offsetY-cy)/sy;
+    console.log(sx+" "+sy);
+    for (var i=0; i<data[0].length; i++)
+    {
+        if (data[0][i][2])
+            continue;
+        if ((Math.abs(data[0][i][0]-mx) <= RADIUS/2.0/sx) && (Math.abs(data[0][i][1]-my) <= RADIUS/2.0/Math.abs(sy)))
+        {
+            selected = i+1;
+            lastpos[0] = mx;
+            lastpos[1] = my;
+            break;
+        }
+    }
+    if (selected == false)
+    {
+        dragging = true;
+        lastpos[0] = e.offsetX;
+        lastpos[1] = e.offsetY;
+    }
+}
+
+function handleMouseMove(e) {
+    if (selected != false)
+    {
+        var mx = (e.offsetX-cx)/sx;
+        var my = (e.offsetY-cy)/sy;
+        data[0][selected-1][0] += mx-lastpos[0];
+        data[0][selected-1][1] += my-lastpos[1];
+        lastpos[0] = mx;
+        lastpos[1] = my;
+        // call physics code here
+    } else if (dragging)
+    {
+        var mx = e.offsetX;
+        var my = e.offsetY;
+        cx += mx-lastpos[0];
+        cy += my-lastpos[1];
+        lastpos[0] = mx;
+        lastpos[1] = my;
+
+    }
+}
+
+function handleMouseUp(e) {
+    selected = false;
+    dragging = false;
+}
+
+function handleMouseOut(e) {
+    selected = false;
+    dragging = false;
+    hilight = false;
+}
+
+var currentKeysDown = {};
+function handleKeyDown(event) {
+    currentKeysDown[event.keyCode] = true;
+    handleKeyPress(event);
+    switch(event.keyCode){
+        case 37:
+        case 38:
+        case 39:
+        case 40:
+            event.preventDefault();
+            break;
+    }
+}
+
+function handleKeyUp(event) {
+    currentKeysDown[event.keyCode] = false;
+}
+
+function handleKeyPress(event) {
+    // left
+    if (event.keyCode == 37) {
+    }
+
+    // right
+    if (event.keyCode == 39) {
+    }
+
+    // up
+    if (event.keyCode == 38) {
+        sx /= 0.9;
+        sy /= 0.9;
+    }
+
+    // down
+    if (event.keyCode == 40) {
+        sx *= 0.9;
+        sy *= 0.9;
+    }
+
+    if (event.keyCode == 'A'.charCodeAt()) {
+        sx /= 0.9;
+        sy /= 0.9;
+        cx = VIEW_WIDTH/2 + (cx-VIEW_WIDTH/2)/0.9
+        cy = VIEW_HEIGHT/2 + (cy-VIEW_HEIGHT/2)/0.9
+    }
+
+    if (event.keyCode == 'Z'.charCodeAt()) {
+        sx *= 0.9;
+        sy *= 0.9;
+        cx = VIEW_WIDTH/2 + (cx-VIEW_WIDTH/2)*0.9
+        cy = VIEW_HEIGHT/2 + (cy-VIEW_HEIGHT/2)*0.9
+    }
+}
+
+var angspeed = 0.01;
+var speed = 0.1;
+function handleKeys() {
+    var m = deltaTime/(1000.0/60);
+    // if (currentKeysDown['W'.charCodeAt()])
+    // {
+    // }
+
+    // left
+    if (currentKeysDown[37]) {
+        // ply.x -= PLY_SPEED*m;
+    }
+
+    // right
+    if (currentKeysDown[39]) {
+        // ply.x += PLY_SPEED*m;
+    }
+
+    // up
+    if (currentKeysDown[38]) {
+        // ply.y -= PLY_SPEED*m;
+    }
+
+    // down
+    if (currentKeysDown[40]) {
+        // ply.y += PLY_SPEED*m;
+    }
 }
 
 var lastTime = 0;
-
-function animate() {
+var deltaTime = 0;
+function updateAll() {
     var timeNow = new Date().getTime();
+    // console.log(timeNow+" "+lastTime+" "+deltaTime)
     if (lastTime != 0) {
-        var elapsed = timeNow - lastTime;
-
+        deltaTime = timeNow - lastTime;
     }
     lastTime = timeNow;
-    update();
+
+    update()
 }
 
 
 function tick() {
     requestAnimFrame(tick);
-    for (var x=0; x<100; x++)
-    {
-        draw();
-        animate();
-    }
+    updateAll();
+    draw();
 }
 
 // in case there's no console
